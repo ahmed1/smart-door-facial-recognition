e305740133b8e374c91f57548561e36397ea36d6
Ahmed Shoukr <ahmedshoukr10@gmail.com>
2020-10-27 13:07:04 -0700

    added proper return headers for functions integrated with api

--- smart-door-serverless/SD-LF2/app.py
+++ smart-door-serverless/SD-LF2/app.py
@@ -35,8 +35,10 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST'
+        },
+        "body": str(res)
     }
